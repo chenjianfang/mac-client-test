4e802ac569ef9d933416b9630fe5112e745d5b94
jianfang.chen <jianfang.chen@pixocial.com>
2022-08-23 16:47:44 +0800

    xx

--- src/client-msg.js
+++ src/client-msg.js
@@ -1,27 +1,20 @@
 console.log('js start run');
 
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 50,
-  eyeColor: "blue",
-};
-
-function postMessage(message, callBack) {
-  console.log('1.1');
-  message.callBack = "(function foo(args) {\n  console.log('call back is invoked' );\n  console.log(args);\n})('hello world')";
-  console.log('1.2');
+export const macMessage = (data = {}, callback) => {
   try {
-    window.webkit.messageHandlers.Native.postMessage(message);
-  } catch(err) {
+    const clientCallbackId = `${Math.floor(Math.random() * 1000)}_${+new Date()}`;
+    data.callback = clientCallbackId;
+    window[clientCallbackId] = callback;
+
+    window.webkit.messageHandlers.Native.postMessage(data);
+  } catch (err) {
     console.log(err);
   }
-}
+};
 
-
-postMessage(person, function (args) {
-  console.log('call back is invoked' );
-  console.log(args);
+const data = {
+  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcGVuX2lkIjoiY2MxaWVlMHZqYmk0aGIybnY5c2ciLCJ0YWciOiIxMVd1bURNcnJ0UUhQdEllTnFUZTNRaCIsInNjb3BlIjoib3Blbl9pZCBwcm9maWxlIGVtYWlsICIsIm1vZGUiOjAsImF1ZCI6InVmNmNqZ2ZibHA0am5wZ2JlaWMxaiIsImV4cCI6MTY2MTI1MTUxMiwiaWF0IjoxNjYxMjQ0MzEyLCJpc3MiOiJodHRwczovL3Bhc3Nwb3J0LmJldGEucGl4b2NpYWwuY29tIn0.MQT7WqRgx0gfyY7SqFVE1_TlQCV7qMWpJEeHV-TblC0",
+};
+macMessage(data, (...args) => {
+  console.log('mac invoke: ', args);
 });
-
-console.log('js start end');
