970bcadd4ddabaffe7797ecd50d99f57b8e7c0a6
jianfang.chen <jianfang.chen@pixocial.com>
2022-08-23 11:34:57 +0800

    feat: 测试通信

--- src/client-msg.js
+++ src/client-msg.js
@@ -1,1 +1,21 @@
-alert('js 执行')
+console.log('js start run');
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+
+function postMessage(message, callBack) {
+  message.callBack = callBack.toString();
+  window.webkit.messageHandlers.Native.postMessage(message);
+}
+
+
+postMessage(person, function (args) {
+  console.log('call back is invoked' );
+  console.log(args);
+});
+
+console.log('js start end');
