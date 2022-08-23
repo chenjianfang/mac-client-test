e6138e690fecbcad55765bb785e913b73234f7eb
jianfang.chen <jianfang.chen@pixocial.com>
2022-08-23 17:40:18 +0800

    feat

--- src/client-msg.js
+++ src/client-msg.js
@@ -1,11 +1,16 @@
 console.log('js start run');
 
-export const macMessage = (type, data = {}, callback) => {
+export const macMessage = (type, params = {}, callback = () => {}) => {
   try {
     const clientCallbackId = `${Math.floor(Math.random() * 1000)}_${+new Date()}`;
-    data.callback = clientCallbackId;
-    data.type = type;
     window[clientCallbackId] = callback;
+
+    // 传给客户端数据
+    const data = {
+      type,
+      callback: clientCallbackId,
+      data: params
+    };
 
     window.webkit.messageHandlers.Native.postMessage(data);
   } catch (err) {
@@ -13,8 +18,8 @@ export const macMessage = (type, data = {}, callback) => {
   }
 };
 
-
 macMessage('login', {
+  
   token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcGVuX2lkIjoiY2MxaWVlMHZqYmk0aGIybnY5c2ciLCJ0YWciOiIxMVd1bURNcnJ0UUhQdEllTnFUZTNRaCIsInNjb3BlIjoib3Blbl9pZCBwcm9maWxlIGVtYWlsICIsIm1vZGUiOjAsImF1ZCI6InVmNmNqZ2ZibHA0am5wZ2JlaWMxaiIsImV4cCI6MTY2MTI1MTUxMiwiaWF0IjoxNjYxMjQ0MzEyLCJpc3MiOiJodHRwczovL3Bhc3Nwb3J0LmJldGEucGl4b2NpYWwuY29tIn0.MQT7WqRgx0gfyY7SqFVE1_TlQCV7qMWpJEeHV-TblC0",
 }, (...args) => {
   console.log('mac invoke: ', args);
